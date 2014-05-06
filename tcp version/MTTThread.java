550730eb3aef7684d32307d64a1c50dab113b434
rahulratthen <rahulratthen.theguy@gmail.com>
2014-05-06 12:17:41 -0500

    submission version

--- tcp version/MTTThread.java
+++ tcp version/MTTThread.java
@@ -27,22 +27,7 @@ public class MTTThread implements Runnable {
 				//replace it with the exponential distribution RV
 				
 				//System.out.println("MTT :"+(int)(rv.expRv(parentThread.mtt)*1000));
-				if(parentThread.mSelfNodeID == 0)
-				{
-				try {
-					File file  = new File("MTTLog.txt");
-					FileWriter fw = new FileWriter(file,true);
-					BufferedWriter bw = new BufferedWriter(fw);
-					bw.write(Integer.toString((int)(rv.expRv(parentThread.mtt))));
-					bw.write("\n");
-					bw.close();
-					fw.close();
-				}
-				catch(Exception ex)
-				{
-					
-				}
-				}
+				
 				Thread.sleep((int)(rv.expRv(parentThread.mtt)));
 				//Thread.sleep(20);
 				parentThread.MTTExpired = true;
